d97aabaa59eea3d98b4879735e48ef446d322126
idandaniels <idandaniels@gmail.com>
2017-11-06 14:34:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from IPython.display import clear_output
-
+import random
 
 board = ['undefine', 'O', 'O','O', 'X', 'X', 'X', 'O', 'O', 'O']
 
@@ -23,16 +23,34 @@ def player_input():
         marker = input('Do you want it to be x or o?').upper()
 
     if marker == 'X':
-        print('X', 'O')
+        return ('X', 'O')
     else:
-        print('O', 'X')
+        return('O', 'X')
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(board, marker):
+    return ((board[7] == marker and board[8] == marker and board[9] == marker) or
+    (board[4] == marker and board[5] == marker and board[6] == marker) or
+    (board[1] == marker and board[2] == marker and board[3] == marker) or
+    (board[7] == marker and board[5] == marker and board[3] == marker) or
+    (board[9] == marker and board[5] == marker and board[1] == marker) or
+    (board[1] == marker and board[4] == marker and board[7] == marker) or
+    (board[9] == marker and board[6] == marker and board[3] == marker))
+
+def choose_first():
+    if random.randint(0,1) ==0:
+        return('Player 1')
+    else:
+        return('Player 2')
 
 
 
 
-player_input()
+
+#player_input()
 #print_board(board)
-
+print(choose_first())
 
 
 
